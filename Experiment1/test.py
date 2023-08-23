c09b862b2d27d6e286ef2de87011d3858e03945e
preranampatil21 <90561253+preranampatil21@users.noreply.github.com>
2023-08-23 23:19:21 +0530

    Update test.py

--- Experiment1/test.py
+++ Experiment1/test.py
@@ -3,7 +3,6 @@ import webapp2
 class Mainpage(webapp2.RequestHandler):
 
 def get(self):
-
-self.response.write("Hello word")
+  self.response.write("Hello word")
 
 app=webapp2.WSGIApplication([('/',Mainpage),],debug=True)
